d84c3ebfb3a1fe36aced8bb7c927870a720c074c
animesh-0041 <ghoroianimesh1508@gmail.com>
2022-12-18 03:10:38 +0530

    day-5

--- script/index.js
+++ script/index.js
@@ -1,121 +1,111 @@
-
 imageSlidergrid();
-function imageSlidergrid(){
-    document.addEventListener("click", (e) => {
-      let handle;
-      if (e.target.matches(".handle")) {
-        handle = e.target;
-      } else {
-        handle = e.target.closest(".handle");
-      }
-      if (handle != null) onHandleClick(handle);
-    });
+function imageSlidergrid() {
+  document.addEventListener("click", (e) => {
+    let handle;
+    if (e.target.matches(".handle")) {
+      handle = e.target;
+    } else {
+      handle = e.target.closest(".handle");
+    }
+    if (handle != null) onHandleClick(handle);
+  });
 
-    const throttleProgressBar = throttle(() => {
-      document.querySelectorAll(".progress-bar").forEach(calculateProgressBar);
-    }, 250);
-    window.addEventListener("resize", throttleProgressBar);
-
+  const throttleProgressBar = throttle(() => {
     document.querySelectorAll(".progress-bar").forEach(calculateProgressBar);
+  }, 250);
+  window.addEventListener("resize", throttleProgressBar);
 
-    function calculateProgressBar(progressBar) {
-      progressBar.innerHTML = "";
-      const slider = progressBar.closest(".row").querySelector(".slider");
-      const itemCount = slider.children.length;
-      const itemsPerScreen = parseInt(
-        getComputedStyle(slider).getPropertyValue("--items-per-screen")
-      );
-      let sliderIndex = parseInt(
-        getComputedStyle(slider).getPropertyValue("--slider-index")
-      );
-      const progressBarItemCount = Math.ceil(itemCount / itemsPerScreen);
+  document.querySelectorAll(".progress-bar").forEach(calculateProgressBar);
 
-      if (sliderIndex >= progressBarItemCount) {
-        slider.style.setProperty("--slider-index", progressBarItemCount - 1);
-        sliderIndex = progressBarItemCount - 1;
-      }
+  function calculateProgressBar(progressBar) {
+    progressBar.innerHTML = "";
+    const slider = progressBar.closest(".row").querySelector(".slider");
+    const itemCount = slider.children.length;
+    const itemsPerScreen = parseInt(
+      getComputedStyle(slider).getPropertyValue("--items-per-screen")
+    );
+    let sliderIndex = parseInt(
+      getComputedStyle(slider).getPropertyValue("--slider-index")
+    );
+    const progressBarItemCount = Math.ceil(itemCount / itemsPerScreen);
 
-      for (let i = 0; i < progressBarItemCount; i++) {
-        const barItem = document.createElement("div");
-        barItem.classList.add("progress-item");
-        if (i === sliderIndex) {
-          barItem.classList.add("active");
-        }
-        progressBar.append(barItem);
-      }
+    if (sliderIndex >= progressBarItemCount) {
+      slider.style.setProperty("--slider-index", progressBarItemCount - 1);
+      sliderIndex = progressBarItemCount - 1;
     }
 
-    function onHandleClick(handle) {
-      const progressBar = handle.closest(".row").querySelector(".progress-bar");
-      const slider = handle.closest(".container").querySelector(".slider");
-      const sliderIndex = parseInt(
-        getComputedStyle(slider).getPropertyValue("--slider-index")
-      );
-      const progressBarItemCount = progressBar.children.length;
-      if (handle.classList.contains("left-handle")) {
-        if (sliderIndex - 1 < 0) {
-          slider.style.setProperty("--slider-index", progressBarItemCount - 1);
-          progressBar.children[sliderIndex].classList.remove("active");
-          progressBar.children[progressBarItemCount - 1].classList.add(
-            "active"
-          );
-        } else {
-          slider.style.setProperty("--slider-index", sliderIndex - 1);
-          progressBar.children[sliderIndex].classList.remove("active");
-          progressBar.children[sliderIndex - 1].classList.add("active");
-        }
+    for (let i = 0; i < progressBarItemCount; i++) {
+      const barItem = document.createElement("div");
+      barItem.classList.add("progress-item");
+      if (i === sliderIndex) {
+        barItem.classList.add("active");
       }
-
-      if (handle.classList.contains("right-handle")) {
-        if (sliderIndex + 1 >= progressBarItemCount) {
-          slider.style.setProperty("--slider-index", 0);
-          progressBar.children[sliderIndex].classList.remove("active");
-          progressBar.children[0].classList.add("active");
-        } else {
-          slider.style.setProperty("--slider-index", sliderIndex + 1);
-          progressBar.children[sliderIndex].classList.remove("active");
-          progressBar.children[sliderIndex + 1].classList.add("active");
-        }
-      }
+      progressBar.append(barItem);
     }
-
-    function throttle(cb, delay = 1000) {
-      let shouldWait = false;
-      let waitingArgs;
-      const timeoutFunc = () => {
-        if (waitingArgs == null) {
-          shouldWait = false;
-        } else {
-          cb(...waitingArgs);
-          waitingArgs = null;
-          setTimeout(timeoutFunc, delay);
-        }
-      };
-
-      return (...args) => {
-        if (shouldWait) {
-          waitingArgs = args;
-          return;
-        }
-
-        cb(...args);
-        shouldWait = true;
-        setTimeout(timeoutFunc, delay);
-      };
-    }
- 
   }
 
+  function onHandleClick(handle) {
+    const progressBar = handle.closest(".row").querySelector(".progress-bar");
+    const slider = handle.closest(".container").querySelector(".slider");
+    const sliderIndex = parseInt(
+      getComputedStyle(slider).getPropertyValue("--slider-index")
+    );
+    const progressBarItemCount = progressBar.children.length;
+    if (handle.classList.contains("left-handle")) {
+      if (sliderIndex - 1 < 0) {
+        slider.style.setProperty("--slider-index", progressBarItemCount - 1);
+        progressBar.children[sliderIndex].classList.remove("active");
+        progressBar.children[progressBarItemCount - 1].classList.add("active");
+      } else {
+        slider.style.setProperty("--slider-index", sliderIndex - 1);
+        progressBar.children[sliderIndex].classList.remove("active");
+        progressBar.children[sliderIndex - 1].classList.add("active");
+      }
+    }
 
+    if (handle.classList.contains("right-handle")) {
+      if (sliderIndex + 1 >= progressBarItemCount) {
+        slider.style.setProperty("--slider-index", 0);
+        progressBar.children[sliderIndex].classList.remove("active");
+        progressBar.children[0].classList.add("active");
+      } else {
+        slider.style.setProperty("--slider-index", sliderIndex + 1);
+        progressBar.children[sliderIndex].classList.remove("active");
+        progressBar.children[sliderIndex + 1].classList.add("active");
+      }
+    }
+  }
 
+  function throttle(cb, delay = 1000) {
+    let shouldWait = false;
+    let waitingArgs;
+    const timeoutFunc = () => {
+      if (waitingArgs == null) {
+        shouldWait = false;
+      } else {
+        cb(...waitingArgs);
+        waitingArgs = null;
+        setTimeout(timeoutFunc, delay);
+      }
+    };
+
+    return (...args) => {
+      if (shouldWait) {
+        waitingArgs = args;
+        return;
+      }
+
+      cb(...args);
+      shouldWait = true;
+      setTimeout(timeoutFunc, delay);
+    };
+  }
+}
 
 //faq javascrip here
 faq();
-function faq(){
- 
-  let accordioItemHeader = document.querySelectorAll(
-    ".accordion-item-header"
-  );
+function faq() {
+  let accordioItemHeader = document.querySelectorAll(".accordion-item-header");
   accordioItemHeader.forEach((accordioItemHeader) => {
     accordioItemHeader.addEventListener("click", (event) => {
       accordioItemHeader.classList.toggle("active");
@@ -128,3 +118,59 @@ function faq(){
     });
   });
 }
+
+// sign in logic
+
+let signInBtn = document.getElementById("sign-in-btn");
+let signInFormContainer = document.querySelector(".sign-in-form-container");
+let overLap = document.querySelector(".over-lap");
+signin();
+function signin() {
+  signInBtn.addEventListener("click", () => {
+    overLap.classList.add("show-over-lap");
+    signInFormContainer.classList.add("active-show-sign-form");
+    signInFormContainer.classList.remove("close-animate");
+    document.body.style.overflowY = "hidden";
+  });
+}
+let closBtn=document.getElementById("close");
+close();
+function close(){
+closBtn.addEventListener("click",()=>{
+  overLap.classList.remove("show-over-lap");
+  signInFormContainer.classList.remove("active-show-sign-form");
+  signInFormContainer.classList.add("close-animate");
+  document.body.style.overflowY = "scroll";
+  
+})
+
+}
+
+
+
+// sign up free trial
+
+// let signUpbtn = document.getElementById("strat-free-trial-btn");
+// let signUpFormContainer = document.querySelector(".sign-up-form-container");
+// let overLap2 = document.querySelector(".over-lap");
+// signup();
+// function signup() {
+//   signUpbtn.addEventListener("click", () => {
+//     overLap.classList.add("show-over-lap");
+//     signUpFormContainer.classList.add("active-show-sign-form");
+//     signUpFormContainer.classList.remove("close-animate");
+//     document.body.style.overflowY = "hidden";
+//   });
+// }
+// let clos2Btn=document.getElementById("close2");
+// close2();
+// function close2(){
+// clos2Btn.addEventListener("click",()=>{
+//   overLap.classList.remove("show-over-lap");
+//   signUpFormContainer.classList.remove("active-show-sign-form");
+//   signUpFormContainer.classList.add("close-animate");
+//   document.body.style.overflowY = "scroll";
+  
+// })
+
+// }
--- script/search.js
+++ script/search.js
@@ -1,53 +1,96 @@
-let moviegrid=document.getElementById("movie-grid");
-let search=document.getElementById("search");
-let actualData=[];
+let moviegrid = document.getElementById("movie-grid");
+let search = document.getElementById("search");
+let actualData = [];
 Fetch();
-async function Fetch(){
-    try {
-        let res=await fetch("search_page_data.json")
-        let actual=await res.json();
-        actualData=actual.searchData;
-        print(actualData);
-        
-    } catch (error) {
-        console.log(error)
-    }
+async function Fetch() {
+  try {
+    let res = await fetch("search_page_data.json");
+    let actual = await res.json();
+    actualData = actual.searchData;
+    // console.log(actual);
+    print(actualData);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function print(data){
-    moviegrid.innerHTML=null;
-    data.forEach(element => {
-        let div=document.createElement("div");
-        let img=document.createElement("img");
-        img.setAttribute("src",element.img);
-        div.append(img);
-        moviegrid.append(div)
-        
-    });
+function print(data) {
+  moviegrid.innerHTML = null;
+  data.forEach((element) => {
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    img.setAttribute("src", element.img);
+    div.append(img);
+    moviegrid.append(div);
+  });
 }
 
 searchMe();
 
-function searchMe(){
-    search.addEventListener("input",(e)=>{
-        let searchValue=e.target.value;
-        let searchReasult = actualData.filter((el) => {
-            return el.name.toLowerCase().includes(searchValue.toLowerCase());
-          });
-          if(searchReasult.length==[]){
-          notFound(searchValue);
-          }else{
-            print(searchReasult);
-          }
-         
-    })
+function searchMe() {
+  search.addEventListener("input", (e) => {
+    let searchValue = e.target.value;
+    let searchReasult = actualData.filter((el) => {
+      return el.name.toLowerCase().includes(searchValue.toLowerCase());
+    });
+    if (searchReasult.length == []) {
+      notFound(searchValue);
+    } else {
+      print(searchReasult);
+    }
+  });
 }
-function notFound(na){
-    moviegrid.innerHTML=null;
-    let div=document.createElement("div");
-    div.setAttribute("id","notfound")
-    div.innerText="No result"+"\n"+`There were no result for${na}`;
-    moviegrid.append(div)
+function notFound(na) {
+  moviegrid.innerHTML = null;
+  let div = document.createElement("div");
+  div.setAttribute("id", "notfound");
+  div.innerText = "No result" + "\n" + `There were no result for${na}`;
+  moviegrid.append(div);
+}
 
+//sign in form
+let signInBtn = document.getElementById("sign-in-btn");
+let signInFormContainer = document.querySelector(".sign-in-form-container");
+let overLap = document.querySelector(".over-lap");
+signin();
+function signin() {
+  signInBtn.addEventListener("click", () => {
+    overLap.classList.add("show-over-lap");
+    signInFormContainer.classList.add("active-show-sign-form");
+    signInFormContainer.classList.remove("close-animate");
+    document.body.style.overflowY = "hidden";
+  });
 }
+let closBtn = document.getElementById("close");
+close();
+function close() {
+  closBtn.addEventListener("click", () => {
+    overLap.classList.remove("show-over-lap");
+    signInFormContainer.classList.remove("active-show-sign-form");
+    signInFormContainer.classList.add("close-animate");
+    document.body.style.overflowY = "scroll";
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
